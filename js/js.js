6de5491bffeb7352f1d25ed1e2c69a6ae034076e
unknown <rezaahmadzadeh0123@gmai.com>
2023-09-20 18:25:50 +0430

    first commit crate navy bar

--- js/js.js
+++ js/js.js
@@ -1,18 +1,4 @@
-const countdownDate = new Date("2023-12-31 00:00:00").getTime();
-
-function updateCountdown() {
-  const now = new Date().getTime();
-  const timeLeft = countdownDate - now;
-
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-  document.getElementById("days").textContent = days.toString().padStart(2, '0');
-  document.getElementById("hours").textContent = hours.toString().padStart(2, '0');
-  document.getElementById("minutes").textContent = minutes.toString().padStart(2, '0');
-  document.getElementById("seconds").textContent = seconds.toString().padStart(2, '0');
-}
-
-setInterval(updateCountdown, 1000);
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
