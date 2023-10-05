be2b94c3fc50616273c3e4b2964d3fb3ee914435
unknown <rezaahmadzadeh0123@gmai.com>
2023-10-05 16:26:42 +0330

    10

--- js/UserManage.js
+++ js/UserManage.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, []);
-  });
+   });
 
 
 
- 
+ const AddUserButton = document.querySelector('#AddUserButton')
+ AddUserButton.addEventListener('click' , ()=>{
+  
+document.querySelector('.AddUser-Modal-Back').classList.remove("dis-none")
+document.querySelector('.AddUser-Modal').classList.add("show")
+
+ });
+ document.querySelector('.AddUser-Modal-Back').addEventListener('click' , (e)=>{
+  
+e.target.classList.add("dis-none")
+document.querySelector('.AddUser-Modal').classList.remove("show")
+
+ });
